fe5d8b8f00fe70327f9a84017e6c1d9fa0cf4c2d
cabemaiwaie <cabemaiwaie@stu.otc.school.nz>
2022-07-29 09:03:43 +1200

    testing

---
+++ Study Planner.py
@@ -0,0 +1,12 @@
+#Set
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("My Study Planner")
+
+main_frame = Frame(root, height=100, width=500)
+intro = Label(main_frame, text="Welcome to My Study Planner")
+
+
+root.mainloop()
