d6c8599fdb2430c308d21fdaa5a418f88893a26e
cabemaiwaie <cabemaiwaie@stu.otc.school.nz>
2022-08-10 13:40:11 +1100

    Made changes to set up so buttons could be placed where I would like them to be

--- Study Planner.py
+++ Study Planner.py
@@ -4,23 +4,23 @@ from tkinter import ttk
 
 root = Tk()
 root.title("My Study Planner")
-
-
-main_frame = Frame(root, height=250, width=250)
-main_frame.pack_propagate(0)
-main_frame.pack(padx=5, pady=5)
+root.geometry('350x500')
+main_frame = Frame(root, height=150, width=100)
+main_frame.pack()
 
 intro = Label(main_frame, text="Welcome to My Study Planner")
-intro.pack(side=TOP)
-
+intro.pack(side=TOP, padx=5, pady=5)
 
 calendar_button = ttk.Button(main_frame, text="Calendar")
-calendar_button.pack()
+calendar_button.pack(padx=5, pady=5)
 
 goals_button = ttk.Button(main_frame, text="Goals")
-goals_button.pack()
+goals_button.pack(padx=5, pady=5)
 
 deadline_button = ttk.Button(main_frame, text="Deadlines")
-deadline_button.pack()
+deadline_button.pack(padx=5, pady=5)
+
+back_button = ttk.Button(root, text="Back")
+back_button.place(x=10, y=470)
 
 root.mainloop()
