b9ef7c0984a0837885529f1228765d7799304e06
cabemaiwaie <cabemaiwaie@stu.otc.school.nz>
2022-08-15 13:02:06 +1200

    Goals page opens up when goals button is clicked

--- Study Planner.py
+++ Study Planner.py
@@ -1,4 +1,6 @@
-
+# This will import all the widgets
+# and modules which are available in
+# tkinter and ttk module
 from tkinter import *
 from tkinter import ttk
 
@@ -11,16 +13,30 @@ main_frame.pack()
 intro = Label(main_frame, text="Welcome to My Study Planner")
 intro.pack(side=TOP, padx=5, pady=5)
 
-calendar_button = ttk.Button(main_frame, text="Calendar")
+# Fuction to open new window on button click
+def create_page():
+    goals_page = Toplevel(root)
+    goals_page.geometry('350x500')
+    goals_page.title("Goals")
+    goals_page.grab_set()
+
+calendar_button = Button(main_frame, text="Calendar", bg="lightblue")
 calendar_button.pack(padx=5, pady=5)
 
-goals_button = ttk.Button(main_frame, text="Goals")
+goals_button = Button(main_frame, text="Goals", bg="lightblue", command=create_page)
 goals_button.pack(padx=5, pady=5)
 
-deadline_button = ttk.Button(main_frame, text="Deadlines")
+deadline_button = Button(main_frame, text="Deadlines", bg="lightblue")
 deadline_button.pack(padx=5, pady=5)
 
-back_button = ttk.Button(root, text="Back")
+back_button = Button(root, text="Back", bg="lightblue")
 back_button.place(x=10, y=470)
+
+
+
+
+
+
+
 
 root.mainloop()
