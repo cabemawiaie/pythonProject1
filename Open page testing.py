16e1137840eab885030009e5d1bc991a62649122
cabemaiwaie <cabemaiwaie@stu.otc.school.nz>
2022-08-22 12:33:24 +1200

    Goals page, calendar page, deadline page all open up when specified button is clicked. From here will create a branch where I will modify functions to do what they currently do now but less repetitive.

--- Open page testing.py
+++ Open page testing.py
@@ -18,17 +18,20 @@ def g_create_page():
     goals_page = Toplevel(root)
     goals_page.geometry('350x500')
     goals_page.title("Goals")
+    goals_page.grabset()
 
 
 def c_create_page():
     calendar_page = Toplevel(root)
     calendar_page.geometry('350x500')
     calendar_page.title("Calendar")
+    calendar_page.grab_set()
 
 def d_create_page():
     deadlines_page = Toplevel(root)
     deadlines_page.geometry('350x500')
     deadlines_page.title("Deadlines")
+    deadlines_page.grab_set()
 
 
 calendar_button = Button(main_frame, text="Calendar", bg="lightblue", command=c_create_page)
@@ -42,3 +45,5 @@ deadline_button.pack(padx=5, pady=5)
 
 back_button = Button(root, text="Back", bg="lightblue")
 back_button.place(x=10, y=470)
+
+root.mainloop()
