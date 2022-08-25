0ec1bf48722552dacebc839f970715ac6f4c88b6
cabemaiwaie <cabemaiwaie@stu.otc.school.nz>
2022-08-26 09:23:49 +1200

    Code does everything previous code did but is less repetitive (improved version).

--- create page from button.py
+++ create page from button.py
@@ -7,41 +7,47 @@ from tkinter import ttk
 def main():
     root = Tk()
     app = Menu(root)
+    root.mainloop()
+
 
 class Menu:
     def __init__(self, master):
-        self.master =master
+        self.master = master
         self.master.title("My Study Planner")
-        self.geometry('350x500')
+        self.master.geometry('350x500')
         self.master.config(bg='light yellow')
-        self.frame = Frame(self.master, height=150, width=100, bg='light yellow')
+        self.frame = Frame(self.master, bg='light yellow')
         self.frame.pack()
 
-        self.intro = Label(main_frame, text="Welcome to My Study Planner")
+        self.intro = Label(self.frame, text="Welcome to My Study Planner")
         self.intro.pack(side=TOP, padx=5, pady=5)
-        
+
         self.btnGoals = Button(self.frame, text="Goals", bg="lightblue", padx=5, pady=5, command=self.new_window)
         self.btnGoals.pack()
-	
+
         self.btnCalendar = Button(self.frame, text="Calendar", bg="lightblue", padx=5, pady=5, command=self.new_window)
         self.btnCalendar.pack()
-	
+
         self.btnDeadline = Button(self.frame, text="Deadlines", bg="lightblue", padx=5, pady=5, command=self.new_window)
         self.btnDeadline.pack()
-	
+
     def new_window(self):
         self.newWindow = Toplevel(self.master)
-        self.app = goals_window(self.newWindow)
+        self.app = goalWindow(self.newWindow)
 
 
-class Goals:
+class goalWindow:
     def __init__(self, master):
-        self.master =master
+        self.master = master
         self.master.title("Goals Page")
-        self.geometry('350x500')
+        self.master.geometry('350x500')
         self.master.config(bg='light yellow')
-        self.frame = Frame(self.master, height=150, width=100, bg='light yellow')
+        self.frame = Frame(self.master, bg='light yellow')
         self.frame.pack()
+
+
+main()
+
 
 
 
