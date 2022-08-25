83cf0f260afeb148d6c4a8153cbdbd287bd549a8
cabemaiwaie <cabemaiwaie@stu.otc.school.nz>
2022-08-26 09:38:54 +1200

    changed colours

--- create page from button.py
+++ create page from button.py
@@ -15,20 +15,20 @@ class Menu:
         self.master = master
         self.master.title("My Study Planner")
         self.master.geometry('350x500')
-        self.master.config(bg='light yellow')
-        self.frame = Frame(self.master, bg='light yellow')
+        self.master.config(bg='yellow1')
+        self.frame = Frame(self.master, bg='yellow1')
         self.frame.pack()
 
-        self.intro = Label(self.frame, text="Welcome to My Study Planner")
-        self.intro.pack(side=TOP, padx=5, pady=5)
+        self.intro = Label(self.frame, text="Welcome to My Study Planner", font=('MS Sans Serif', 10, 'bold'), bg='yellow1')
+        self.intro.pack(side=TOP)
 
-        self.btnGoals = Button(self.frame, text="Goals", bg="lightblue", padx=5, pady=5, command=self.new_window)
+        self.btnGoals = Button(self.frame, text="Goals", bg="wheat1", padx=5, pady=5, command=self.new_window)
         self.btnGoals.pack()
 
-        self.btnCalendar = Button(self.frame, text="Calendar", bg="lightblue", padx=5, pady=5, command=self.new_window)
+        self.btnCalendar = Button(self.frame, text="Calendar", bg="wheat1", padx=5, pady=5, command=self.new_window)
         self.btnCalendar.pack()
 
-        self.btnDeadline = Button(self.frame, text="Deadlines", bg="lightblue", padx=5, pady=5, command=self.new_window)
+        self.btnDeadline = Button(self.frame, text="Deadlines", bg="wheat1", padx=5, pady=5, command=self.new_window)
         self.btnDeadline.pack()
 
     def new_window(self):
@@ -41,8 +41,8 @@ class goalWindow:
         self.master = master
         self.master.title("Goals Page")
         self.master.geometry('350x500')
-        self.master.config(bg='light yellow')
-        self.frame = Frame(self.master, bg='light yellow')
+        self.master.config(bg='yellow1')
+        self.frame = Frame(self.master, bg='yellow1')
         self.frame.pack()
 
 
