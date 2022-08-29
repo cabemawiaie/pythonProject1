80d54194aa4bafe8c3a2c83591a490c41a67a13b
cabemawiaie <72894733+cabemawiaie@users.noreply.github.com>
2022-08-30 10:14:07 +1200

    Update create page from button.py
    Added exit button but currently button only exits on the menu page. Also added functions so calendar and deadline open to their own pages.

--- create page from button.py
+++ create page from button.py
@@ -22,37 +22,73 @@ class Menu:
         self.intro = Label(self.frame, text="Welcome to My Study Planner", font=('MS Sans Serif', 10, 'bold'), bg='yellow1')
         self.intro.pack(side=TOP)
 
-        self.btnGoals = Button(self.frame, text="Goals", bg="wheat1", padx=5, pady=5, command=self.new_window)
+     #==================================Buttons=======================================================================
+        self.btnGoals = Button(self.frame, text="Goals", bg="lightblue", padx=5, pady=5, command=self.goal_page)
         self.btnGoals.pack()
 
-        self.btnCalendar = Button(self.frame, text="Calendar", bg="wheat1", padx=5, pady=5, command=self.new_window)
+        self.btnCalendar = Button(self.frame, text="Calendar", bg="lightblue", padx=5, pady=5, command=self.calendar_page)
         self.btnCalendar.pack()
 
-        self.btnDeadline = Button(self.frame, text="Deadlines", bg="wheat1", padx=5, pady=5, command=self.new_window)
+        self.btnDeadline = Button(self.frame, text="Deadlines", bg="lightblue", padx=5, pady=5, command=self.deadline_page)
         self.btnDeadline.pack()
 
-    def new_window(self):
+
+        self.btnExit = Button(self.master, text="Exit", bg="lightblue", padx=5, pady=5, command=self.Exit)
+        self.btnExit.place(x=10, y=460)
+
+        #self.btnExit = Button(goalWindow(self.newWindow), text="Exit", bg="lightblue")
+        #self.btnExit.place(x=10, y=470)x
+
+
+#==================================Buttons=======================================================================
+
+#==================================Button Functions==============================================================
+    def goal_page(self):
         self.newWindow = Toplevel(self.master)
         self.app = goalWindow(self.newWindow)
 
+    def calendar_page(self):
+        self.newWindow = Toplevel(self.master)
+        self.app = calendarWindow(self.newWindow)
+
+    def deadline_page(self):
+        self.newWindow = Toplevel(self.master)
+        self.app = deadlineWindow(self.newWindow)
+
+    def Exit(self):
+        self.Exit = tkinter.messagebox.askyesno("Exit confirmation", "Confirm if you want to exit")
+        if self.Exit > 0:
+            self.master.destroy()
+        else:
+            command = self.new_window
+            return
+#==================================Button Functions==============================================================
 
 class goalWindow:
     def __init__(self, master):
         self.master = master
-        self.master.title("Goals Page")
+        self.master.title("Your Goals")
         self.master.geometry('350x500')
-        self.master.config(bg='yellow1')
-        self.frame = Frame(self.master, bg='yellow1')
+        self.master.config(bg='light yellow')
+        self.frame = Frame(self.master, bg='light yellow')
         self.frame.pack()
 
+class calendarWindow:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Calendar")
+        self.master.geometry('350x500')
+        self.master.config(bg='light yellow')
+        self.frame = Frame(self.master, bg='light yellow')
+        self.frame.pack()
+
+class deadlineWindow:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Deadlines")
+        self.master.geometry('350x500')
+        self.master.config(bg='light yellow')
+        self.frame = Frame(self.master, bg='light yellow')
+        self.frame.pack()
 
 main()
-
-
-
-
-
-
-
-
-
