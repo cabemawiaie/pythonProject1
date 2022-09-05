85008550ba5ff48225dde4fcf5a600d081004702
cabemaiwaie <cabemaiwaie@stu.otc.school.nz>
2022-09-06 10:52:54 +1200

    Code runs, no errors but does not display anything

--- Open page testing.py
+++ Open page testing.py
@@ -1,11 +1,7 @@
 # This will import all the widgets
 # and modules which are available in
 # tkinter and ttk module
-import tkinter
-from tkinter import *
 import tkinter as tk
-from tkinter import ttk
-
 
 
 # need to explain class
@@ -23,10 +19,8 @@ class StudyApp(tk.Tk):
         self.frames = {}
 
         for F in (Main, GoalPage, ToDoPage):
-            page_name = F.__name__
-            frame = F(parent=container, controller=self)
-            self.frames[page_name] = frame
-            frame.winfo_geometry('350x500')
+            frame = F(container, self)
+            self.frames[F] = frame
 
             # put all of the pages in the same location;
             # the one on the top of the stacking order
@@ -34,52 +28,52 @@ class StudyApp(tk.Tk):
             frame.grid(row=0, column=0, sticky="nsew")
         self.show_frame(Main)
 
-    def show_frame(self, page_name):
+    def show_frame(self, cont):
         # Show window for the given page name
-        frame = self.frames[page_name]
+        frame = self.frames[cont]
         frame.tkraise()
 
 
-class Main(ttk.Frame):
+class Main(tk.Frame):
     def __init__(self, parent, controller):
-        ttk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = ttk.Label(self, text="Main Page", font=('MS Sans Serif', 10, 'bold'))
+        label = tk.Label(self, text="Main Page", font=('MS Sans Serif', 10, 'bold'))
         label.pack(padx=10, pady=10)
 
         # Buttons
-        btn_goals = ttk.Button(self, text="Goals", padx=5, pady=5, command=lambda: controller.show_frame(GoalPage))
+        btn_goals = tk.Button(self, text="Goals", padx=5, pady=5, command=lambda: controller.show_frame(GoalPage))
         btn_goals.pack()
 
-        btn_calendar = ttk.Button(self, text="Calendar",
-                                  command=lambda: controller.show_frame(ToDoPage))
-        btn_calendar.pack(padx=5, pady=5)
+        btn_to_do = tk.Button(self, text="To Do List",
+                              command=lambda: controller.show_frame(ToDoPage))
+        btn_to_do.pack()
 
         # self.btnExit = Button(goalWindow(self.newWindow), text="Exit", bg="lightblue")
         # self.btnExit.place(x=10, y=470)x
 
 
-class GoalPage(ttk.Frame):
+class GoalPage(tk.Frame):
     def __init__(self, parent, controller):
-        ttk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = ttk.Label(self, text="Your Goals", font=('MS Sans Serif', 10, 'bold'))
+        label = tk.Label(self, text="Your Goals", font=('MS Sans Serif', 10, 'bold'))
         label.pack(pady=10, padx=10)
 
-        btn_exit = ttk.Button(self, text="Back to Main Page",
-                              command=lambda: controller.show_frame(Main))
-        btn_exit.place(x=10, y=460, padx=5, pady=5)
+        btn_exit = tk.Button(self, text="Back to Main Page",
+                             command=lambda: controller.show_frame(Main))
+        btn_exit.place(x=10, y=460)
 
 
-class ToDoPage(ttk.Frame):
+class ToDoPage(tk.Frame):
     def __init__(self, parent, controller):
-        ttk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = ttk.Label(self, text="To Do List", font=('MS Sans Serif', 10, 'bold'))
+        label = tk.Label(self, text="To Do List", font=('MS Sans Serif', 10, 'bold'))
         label.pack(pady=10, padx=10)
 
-        btn_exit = ttk.Button(self, text="Back to Main Page",
-                              command=lambda: controller.show_frame(Main))
+        btn_exit = tk.Button(self, text="Back to Main Page",
+                             command=lambda: controller.show_frame(Main))
         btn_exit.place(x=10, y=460)
 
 
